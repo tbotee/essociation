0076d510a1177061d7946025fe877379be792b92
Botond Tánczos <botond.tanczos@aplnexted.com>
2024-10-14 13:17:40 +0300

    Association Table Fix

--- app/Livewire/Association/AssociationList.php
+++ app/Livewire/Association/AssociationList.php
@@ -5,11 +5,14 @@ namespace App\Livewire\Association;
 use App\Models\Association;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Livewire\Component;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Filters\Filter;
+use Illuminate\View\View;
 
 class AssociationList extends Component implements HasForms, HasTable
 {
@@ -20,6 +23,7 @@ class AssociationList extends Component implements HasForms, HasTable
     {
         return $table
             ->query(Association::query())
+            ->defaultGroup('region.name')
             ->columns([
                 TextColumn::make('name')
                     ->label(__('name'))
@@ -34,11 +38,38 @@ class AssociationList extends Component implements HasForms, HasTable
                     ->searchable(),
                 TextColumn::make('phone')
                     ->label(__('phone'))
-            ]);
+            ])
+            ->emptyStateHeading(__('no_associations'))
+            ->headerActions([
+                Action::make('Add New')
+                    ->label('Add New')
+                    ->icon('heroicon-o-plus') // You can choose an icon
+                    ->color('primary') // Choose color
+                    ->action(fn () => $this->openModal()), // Define your action
+            ])
+            ->heading(__('associations'))
+            ->deferLoading();
+
+    }
+
+    protected function getHeader(): View
+    {
+        return view('filament.association.table-header', [
+            'addNewButton' => Action::make('Add New')
+                ->label('Add New')
+                ->icon('heroicon-o-plus')
+                ->color('primary')
+                ->action(fn () => $this->openModal()),
+        ]);
     }
 
     public function render()
     {
         return view('livewire.association.association-list');
+    }
+
+    protected function openModal()
+    {
+        $this->dispatch('open-modal', 'add-association');
     }
 }
--- resources/views/livewire/association/association-list.blade.php
+++ resources/views/livewire/association/association-list.blade.php
@@ -1,3 +1,14 @@
 <div>
     {{ $this->table }}
+
+    <x-modal name="add-association" :show="$errors->isNotEmpty()" focusable>
+        <div class="mt-6 flex justify-end">
+            <form wire:submit="deleteUser" class="p-6">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+            </form>
+        </div>
+    </x-modal>
+
 </div>
